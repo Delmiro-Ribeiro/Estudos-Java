850126208aa73e686148faf7800dd25cafd71ef1
Delmiro-Ribeiro <delmiroribeiro.alpha@gmail.com>
2022-09-25 12:31:57 -0300

    Informações básicas sobre importações

---
+++ Exercicios/src/Fundamentos/Import.java
@@ -0,0 +1,13 @@
+package Fundamentos;
+
+import java.util.Date;
+/*
+ * Nem tudo está disponivel dentro da linguagem java atraves do JDK
+ * ten-se a nescessidade de importar ferramentas externas para uso interno ou externo.
+ */
+public class Import {
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println(d);
+    }
+}
