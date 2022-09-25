199710fc9633053fbbf2f65ae7c4ecf30ec8b8cb
Delmiro-Ribeiro <delmiroribeiro.alpha@gmail.com>
2022-09-25 12:51:21 -0300

    Um pouco sobre String

---
+++ Exercicios/src/Fundamentos/TipoString.java
@@ -0,0 +1,15 @@
+package Fundamentos;
+
+public class TipoString {
+    public static void main(String[] args) {
+        System.out.println("olá pessoal".charAt(2));//pede o index da string.
+
+        String s = "Boa tarde"; //String é um objeto de valor imutavel
+        System.out.println(s.length());
+
+        //Muitas funções String disponiveis com a notação ponto , 
+        // que apesar de querer colocar aqui só que ficará algo muito massante
+        //comando para verificar as opções de funcionalidade e acima : S.(funcionalidade a ser escolida).
+
+    }
+}
