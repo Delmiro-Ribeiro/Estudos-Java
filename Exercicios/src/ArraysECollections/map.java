201bd4295edc6719d574394684c56abd12d15694
Delmiro-Ribeiro <delmiroribeiro.alpha@gmail.com>
2022-10-12 22:30:02 -0300

    Filas no java

---
+++ Exercicios/src/ArraysECollections/map.java
@@ -0,0 +1,16 @@
+package ArraysECollections;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class map {
+    // map refere-se a chave e valor
+    Map<Integer, String> usuarios = new HashMap<>();
+
+    usuarios.put(1,"Diego");
+    usuarios.put(2,"Diva");
+    usuarios.put(3,"livia");
+    usuarios.put(4,"adressa");
+
+}
