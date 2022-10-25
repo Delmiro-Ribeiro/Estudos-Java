92de4493131b37e6c316f2388b7daf9704794508
Delmiro Ribeiro <delmiroribeiro.alpha@gmail.com>
2022-10-25 15:14:55 -0300

    lista de exercicios

---
+++ Exercicios/src/listaDeExercicios/ex001.java
@@ -0,0 +1,8 @@
+package listaDeExercicios;
+//Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+
+public class ex001{
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+    }
+}
