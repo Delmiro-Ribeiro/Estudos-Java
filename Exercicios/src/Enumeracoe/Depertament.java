f4ae7098b41c86e4b2ab92d780635a351724f2c1
Delmiro Ribeiro <98195526+Delmiro-Ribeiro@users.noreply.github.com>
2023-03-30 21:10:54 -0300

    Feature001 (#8)
    * Exercicio de enumerações java
    
    * upate
    
    * upate 2

---
+++ Exercicios/src/Enumeracoe/Depertament.java
@@ -0,0 +1,20 @@
+package Enumeracoe;
+
+public class Depertament {
+
+    private String name;
+
+    public Departament(){
+    }
+    public Depertament(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
