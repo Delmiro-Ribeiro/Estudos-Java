6bd74665ee939bf8898310d37268113846489d6e
Delmiro Ribeiro <delmiroribeiro.alpha@gmail.com>
2023-06-22 23:08:57 -0300

    studet java NA interfaces

---
+++ Exercicios/src/JavaNA/Application/Program.java
@@ -0,0 +1,17 @@
+package JavaNA.Application;
+
+import java.util.Scanner;
+
+//public class Program {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Entre com os dados do aluguel");
+//
+//
+//
+//        sc.close();
+//
+//    }
+//
+//}
